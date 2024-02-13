ac45747126a03dd0b4a0ffedc8b804bfcd9886f9
Sahan Fernando <sahanvindana@gmail.com>
2024-02-13 19:42:24 +0100

    Build game attributes component

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,10 +1,14 @@
+import { Genre } from "./Genre";
 import { PlatForm } from "./PlatForm";
+import { Publisher } from "./Publisher";
 
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: PlatForm; }[];
